b2887f1a8e28f05b382a81bee320b16d61a32028
Dustboy743 <dsaidns@gmail.com>
2025-09-08 23:17:01 +0530

    java

--- vars/java_eks.groovy
+++ vars/java_eks.groovy
@@ -4,7 +4,7 @@ def call(Map configMap){
                 label 'agent-1'
             }
             tools {
-                nodejs 'maven'
+                maven 'maven'
             }
 
             environment {
